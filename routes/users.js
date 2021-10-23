eb3805dd5920ce26198a4e8d2cfb08c9416b14e9
sujnbist <sujnbist@gmail.com>
2021-10-23 10:53:52 +0545

    add getallusers api rename script dev to start

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,15 @@ const router = express.Router();
 require('../db/conn');
 const User = require('../model/userSchema')
 // Get Requests
-router.get('/', (req, res) => {
-    queryparam = req.query.data;
-
-    res.json({
-        data: queryparam
-    })
+router.get('/', async (req, res) => {
+    try {
+        const userlist = await User.find();
+        return res.status(200).json({
+            data: userlist
+        })
+    } catch (ex) {
+        return res.status(500).json({ data: ex.message() })
+    }
 });
 
 // Get Requests
@@ -49,33 +52,33 @@ router.post('/', (req, res) => {
 // Post requests
 router.post('/register', async (req, res) => {
 
-    try{
-    const { name, email, phone, work, password, cpassword } = req.body;
-    console.log(req.body);
+    try {
+        const { name, email, phone, work, password, cpassword } = req.body;
+        console.log(req.body);
 
-    if (!name || !email || !phone || !work || !password || !cpassword) {
-        return res.status(422).json({ error: " fill the form" })
+        if (!name || !email || !phone || !work || !password || !cpassword) {
+            return res.status(422).json({ error: " fill the form" })
+        }
+        const userExist = await User.findOne({ email })
+        if (userExist) return res.status(422).json({ error: "User already Exist" })
+        const savedUser = new User({ name, email, phone, work, password, cpassword }).save();
+        if (savedUser) return res.status(200).json({ data: savedUser })
+    } catch (ex) {
+        return res.status(500).json({ data: ex.message() })
     }
-    const userExist = await User.findOne({email})
-    if(userExist) return res.status(422).json({error: "User already Exist"})
-    const savedUser = new User({name, email, phone, work, password, cpassword}).save();
-    if(savedUser) return res.status(200).json({data: savedUser})
-}catch(ex){
-    return res.status(500).json({data: ex.message()})
-}
 });
 
-router.post('/login', async (req,res) => {
-   
-    try{
-        const{email, password} = req.body;
-        const user = await User.findOne({email})
-        if(!user) return res.status(404).json({data: "user no existo"})
-        const passwordcheck = user.password === password;
-        passwordcheck ? res.status(200).json({data: "wwelcome"}) : res.status(400).json({data: "invalid attempt"});
+router.post('/login', async (req, res) => {
 
-    }catch(ex){
-        return res.status(500).json({data: ex.message()})
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email })
+        if (!user) return res.status(404).json({ data: "user no existo" })
+        const passwordcheck = user.password === password;
+        passwordcheck ? res.status(200).json({ data: "wwelcome" }) : res.status(400).json({ data: "invalid attempt" });
+
+    } catch (ex) {
+        return res.status(500).json({ data: ex.message() })
     }
 })
 module.exports = router;
